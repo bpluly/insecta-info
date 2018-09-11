2d9d661b49216d24cc7fa53ad8c30d85d4275547
Simon Lucy <simon.lucy@gravit.as>
2018-09-11 12:33:44 +0100

    rstrip() not trim()

--- backend/services/importNHME.py
+++ backend/services/importNHME.py
@@ -203,7 +203,7 @@ def main():
       allFields = next(NHMReader)
       for field in allFields:
         fieldList += doublequote(field)+","
-      fieldList = fieldList.trim(",")
+      fieldList = fieldList.rstrip(',')
       insertStringbase = """INSERT INTO "NHM_Occurrence" ("""+fieldList+") VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
       print(insertStringbase)
       for row in NHMReader:
