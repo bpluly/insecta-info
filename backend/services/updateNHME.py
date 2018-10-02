423e0cb6ecb1471d196850313cc4af08d36cec8f
Simon Lucy <simon.lucy@gravit.as>
2018-10-02 16:26:15 +0100

    dump the row t the log, check None in array

--- backend/services/updateNHME.py
+++ backend/services/updateNHME.py
@@ -130,7 +130,7 @@ def updateOccurrence(dbConn, dbcursor, fieldList, row):
     if testing == False:
       try:
         logger.info("UPDATE:"+row[0])
-        logger.info(dbcursor.mogrify(updateStringbase, row))
+        print(row)
         dbcursor.execute(updateStringbase, row)
       except psycopg2.OperationalError as e:
         logger.exception("UPDATE FAILED:")
